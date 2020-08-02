f7267e5e70cb1a64a9d106f0213c8a52c5702e92
Nicolas Villetelle <nicolas.villetelle@groupe-cyllene.com>
2020-08-02 11:41:33 +0200

    IOS design patterns and the model-view-controller (MVC)

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let story = storyBrain.nextStory(userChoice: sender.currentTitle!)
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = storyBrain.stories[0].title
+        choice1Button.setTitle(storyBrain.stories[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2, for: .normal)
     }
 
 
